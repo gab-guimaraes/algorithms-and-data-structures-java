8e13294200cf17c543451954da4e6ddc3a49b2f2
Gabriel Guimaraes <dedito182@gmail.com>
2022-07-18 14:32:16 -0300

    adjacency matrix

--- src/main/java/org/johnwick182/graphs/AdjacencyMatrix.java
+++ src/main/java/org/johnwick182/graphs/AdjacencyMatrix.java
@@ -1,5 +1,9 @@
 package org.johnwick182.graphs;
 
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class AdjacencyMatrix {
 
     static int[][] graph;
@@ -14,14 +18,17 @@ public class AdjacencyMatrix {
                 {0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0}
         };
+        System.out.println("size of the graph is: " + graph.length);
         dfs(0, new boolean[graph.length]);
-
     }
-
 
     public static void dfs(int start, boolean[] visited) {
         System.out.println("start... ");
         visited[start] = true;
+        //visited[0] = true;
+        //visited[1] = false;
+        //I'll have an array with n size.
+        //each element is the vertex.
 
         for (int i = 0; i < graph[start].length; i++) {
             if (graph[start][i] == 1 && (!visited[i])) {
@@ -29,6 +36,28 @@ public class AdjacencyMatrix {
                 dfs(i, visited);
             }
         }
+    }
+
+    public static boolean hasCycle(boolean[] visited, int start) {
+        System.out.println("Start BFS... ");
+        Queue<Integer> myQueue = new LinkedList<>();
+        myQueue.add(start); //my first element
+        visited[start] = true; //visiting the start elemetn
+
+        while (!myQueue.isEmpty()) {
+            int element = myQueue.poll();
+            for (int i = 0; i < graph[element].length; i++) {
+                if (graph[element][i] == 1) {
+                    if (visited[element]) {
+                        return true;
+                    } else {
+                        visited[element] = true;
+                        myQueue.add(element);
+                    }
+                }
+            }
+        }
+        return false;
     }
 
 }
